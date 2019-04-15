ab447e6197d615a3bbb7b45aa00882a9ffe78c79
hahamark <m.h.g.romme@gmail.com>
2019-04-15 12:38:35 +0200

    Adapted the reward

--- EmphaticDQN.py
+++ EmphaticDQN.py
@@ -482,7 +482,7 @@ def deep_q_learning(sess,
 tf.reset_default_graph()
 
 # Where we save our checkpoints and graphs
-experiment_dir = os.path.abspath("./experiments/{}".format('limited_resources_correct_run_1.9'))
+experiment_dir = os.path.abspath("./experiments/{}".format('limited_resources_correct_run_1.9.1'))
 
 # Create a glboal step variable
 global_step = tf.Variable(0, name='global_step', trainable=False)
