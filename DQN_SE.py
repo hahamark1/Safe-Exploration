9321e250ea7cfdd2f5b483f5fc2243462854f3c1
hahamark <m.h.g.romme@gmail.com>
2019-05-15 10:46:06 +0200

    Solved issue with headless coins

--- DQN_SE.py
+++ DQN_SE.py
@@ -25,7 +25,7 @@ WINDOW_LENGTH = 4
 IMAGE_SIZE = 128
 MAX_PIXEL = 255.0
 # MAP_MULTIPLIER = 30.9
-EXPERIMENT_NAME = 'safe_exploration_3.1'
+EXPERIMENT_NAME = 'safe_exploration_3.2'
 
 class StateProcessor():
     """
